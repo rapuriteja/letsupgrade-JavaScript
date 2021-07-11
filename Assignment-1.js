db2dce19cba09743437a260dcc37991ab0a49da2
rapuriteja <75659508+rapuriteja@users.noreply.github.com>
2021-07-11 17:37:28 +0530

    Add files via upload

---
+++ Assignment-1.js
@@ -0,0 +1,14 @@
+// JavaScript Essential Assignment-1
+
+// Write a program to Print 5 stars in same line
+
+var s = " "
+for(i =1 ; i<=5; i++){
+    s= s+ "*"
+}
+
+console.log(s);
+
+// Output:
+
+//   *****
